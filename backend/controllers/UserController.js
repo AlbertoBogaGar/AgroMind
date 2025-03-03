23e54bf33aea483139fcd78a35690843f8e0b643
Alberto Boga <alberto.boggar@educa.jcyl.es>
2025-03-03 16:12:11 +0100

    Se crea el Register y el Login en la parte del backen, testeado con PostMan

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,68 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// REGISTRO DE USUARIO
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Verificar si el usuario ya existe
+    const usuarioExistente = await User.findOne({ where: { email } });
+    if (usuarioExistente) {
+      return res.status(400).json({ message: "El correo ya está registrado" });
+    }
+
+    // Hashear la contraseña
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Crear usuario
+    const usuario = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "Usuario registrado correctamente",
+    });
+  } catch (error) {
+    console.error("Error en registro:", error);
+    res
+      .status(500)
+      .json({ message: "Error en el servidor", error: error.message });
+  }
+};
+
+// LOGIN DE USUARIO
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const usuario = await User.findOne({ where: { email } });
+
+    if (!usuario) {
+      return res
+        .status(400)
+        .json({ message: "Usuario no encontrado en base de datos" });
+    }
+
+    const passwordValida = await bcrypt.compare(password, usuario.password);
+    if (!passwordValida) {
+      return res.status(400).json({ message: "Credenciales incorrectas" });
+    }
+
+    const token = jwt.sign(
+      { id: usuario.id, email: usuario.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "2h" }
+    );
+
+    res.json({ message: "Login exitoso", token });
+  } catch (error) {
+    console.error("Error detallado:", error);
+    res
+      .status(500)
+      .json({ message: "Error en el servidor", error: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const sequelize = require("./config/database"); // Importar la base de datos
+const authRoutes = require("./routes/authRoutes");
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("API AgroMind funcionando 🚀");
