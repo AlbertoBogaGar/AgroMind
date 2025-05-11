c2313e102cf7f49663d122865238e857bb30b03f
Alberto Boga <alberto.boggar@educa.jcyl.es>
2025-05-11 17:31:33 +0200

    update cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.options("*", cors());
 app.use(cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
 }));
