a98f8de34262c7a383e6d2a1c83c0d8278204d03
Alberto Boga <alberto.boggar@educa.jcyl.es>
2025-05-11 17:42:13 +0200

    8080

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ sequelize.sync({ force: false })
     .then(() => console.log("Tablas sincronizadas con éxito"))
     .catch(err => console.error("Error al sincronizar tablas:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 console.log("Justo antes del app.listen");
 
 app.listen(PORT, () => {
